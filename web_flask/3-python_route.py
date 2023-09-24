fc6ce817ce29ea18b3bce969bf2429e8894fdd88
horlahuk <olaitan.u.k@gmail.com>
2023-09-24 04:38:58 -0700

    task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -24,6 +24,7 @@ def c_text(text):
     return 'C ' + text.replace('_', ' ')
 
 
+@app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text='is_cool'):
     '''display python followed by param'''
