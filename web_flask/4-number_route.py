185b8d8f0062b8c38e16bb1295fa1a6f836d7597
horlahuk <olaitan.u.k@gmail.com>
2023-09-24 04:50:32 -0700

    task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+'''starts a flask web app'''
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def greet():
+    '''display hello HBNB'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''display HBNB'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    '''display c followed by param passed'''
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is_cool'):
+    '''display python followed by param'''
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def num(n):
+    '''display n is a number'''
+    if type(n) == int:
+        return n + ' is a number'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
